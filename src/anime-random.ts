4f3efe3e4e5bb4c5d346c9de58e4e208d5a05e85
Torio Yoshiko <torio.yoshiko@gmail.com>
2021-06-30 12:45:02 +0200

    Add anime-random

--- src/anime-random.ts
+++ src/anime-random.ts
@@ -1,18 +1,16 @@
 import fetch from 'node-fetch';
 
-const getRandomArbitrary = (min, max) => Math.random() * (max - min) + min;
+const getRandomArbitrary = (min, max) => Math.floor(Math.random() * (max - min) + min);
 
 export const animeRandomizer = async () => {
   const url = 'https://api.jikan.moe/v3/top/anime/';
-  const page = Math.floor(getRandomArbitrary(1, 100));
-  const result = [];
+  const page = getRandomArbitrary(1, 100);
   const anime = Math.floor(getRandomArbitrary(1, 52));
 
   const response = await fetch(url + page.toString());
   const animeList = await response.json();
-  result.push(animeList);
 
-  const finalResult = result[0].top[anime];
+  const finalResult = animeList.top[anime];
 
   const animeResponse = await fetch(`https://api.jikan.moe/v3/anime/${finalResult.mal_id.toString()}`);
   const animeInfo = await animeResponse.json();
